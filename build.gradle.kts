9fb698701b2511e574193fa47f5dcc85826d7070
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2024-06-09 20:40:17 -0700

    Setup project

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.serialization") version "2.0.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+fun kotlinx(module: String, version: String? = null): String =
+    "org.jetbrains.kotlinx:kotlinx-$module${version?.let { ":$it" } ?: ""}"
+
+dependencies {
+    implementation(kotlinx("serialization-json", "1.7.0"))
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
